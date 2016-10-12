8edff584f964809f60bb71cd451f6d9c44ed548f
David <Davidteri91@gmail.com>
2016-10-12 12:42:21 +0200

    sony-common: set LOS Build Name
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- lineage-common.mk
+++ lineage-common.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Lineage Build
+TARGET_UNOFFICIAL_BUILD_ID := SonyLOS
+
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 DEVICE_SPECIFIC_CAMERA_PATH := hardware/sony/camera
