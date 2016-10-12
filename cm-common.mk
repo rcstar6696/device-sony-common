d9d5096d22d74d5da010175aac48355e1b254f46
David <Davidteri91@gmail.com>
2016-10-12 12:42:21 +0200

    sony-common: set CM Build Name
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- cm-common.mk
+++ cm-common.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# CM Build
+TARGET_UNOFFICIAL_BUILD_ID := SonyCM
+
 # Audio
 include vendor/qcom/extras/proprietary.mk
 
