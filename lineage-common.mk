3b47893f7e8dcbf983429c0c10192fd4b0e0a928
David <Davidteri91@gmail.com>
2017-01-03 14:57:12 +0100

    sony-common: add sony flag
    this let's inherit only sony repos
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- lineage-common.mk
+++ lineage-common.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+BOARD_VENDOR := sony
+
 # Lineage Build
 TARGET_UNOFFICIAL_BUILD_ID := SonyLOS
 
